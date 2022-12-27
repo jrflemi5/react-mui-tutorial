1281ccb9aeb2f316b1f997f787cef59f3964854b
unknown <reaganfleming@gmail.com>
2022-12-26 17:16:36 -0700

    typography

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material"
+const Test = () => {
+    return (
+        <div>
+          <Typography> Hello there the angel from my nightmare </Typography>
+        </div>
+    );
+}
+
+
+export default Test
